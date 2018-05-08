68e01b627650853600e7160822a15b5bf2f85920
Eduardo Martins <eduardomartins.ti@hotmail.com>
2018-05-07 22:56:38 -0300

    LASA-Projeto
    - Atualização do arquivo contendo algumas querys realizadas ao decorrer do desenvolvimento.

--- projetoLASA.sql
+++ projetoLASA.sql
@@ -1,31 +1,36 @@
-CREATE DATABASE projetoLASA;
-use projetoLASA;
+CREATE DATABASE vendas;
+use vendas;
 
 CREATE TABLE tb_venda (
 	id_venda int not null,
-	data Date,
+	data timestamp,
     loja int,
     pdv int,
-    status VARCHAR(100),
-	PRIMARY KEY (id_venda)
+    status VARCHAR(100)
 );
 
 CREATE TABLE tb_item_venda (
     id_venda int,
-    id_item_venda int not null,
+    id_item_venda int,
     produto VARCHAR(256),
-	preco_unitario DECIMAL,
-    desconto decimal,
-    PRIMARY KEY (id_item_venda),
-    FOREIGN KEY (id_venda) REFERENCES tb_venda(id_venda)
+	preco_unitario DECIMAL(4,2),
+    desconto DECIMAL(4,2)
 );
 
 CREATE TABLE tb_processamento (
 	id_processamento int not null auto_increment,
-    data Date,
+    data timestamp,
     loja int,
     pdv int,
     nome_arquivo VARCHAR(256),
     status VARCHAR(100),
     PRIMARY KEY (id_processamento)
 );
+
+SELECT * FROM tb_venda;
+SELECT * FROM tb_item_venda;
+SELECT * FROM tb_processamento;
+
+DROP TABLE tb_processamento;
+DROP TABLE tb_venda;
+DROP TABLE tb_item_venda;
